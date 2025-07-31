e0c7f635ea40d0473dd4fea2decd2f1fd3141d11
Isabel Cruz <93488292+bellDataSc@users.noreply.github.com>
2025-07-31 14:40:31 -0300

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -156,7 +156,8 @@ elif menu == 'Tarefas':
 
                 with col2:
                     status_color = {'Novo', 'Em Andamento', 'Concluído'}
-                    st.markdown(f"### {status_color.get(row['status'] '?')} {row['status']}")
+                    
+                    st.markdown(f"### {status_color.get(row['status'], '❓')} {row['status']}")
 
                 with col3:
                     new_status = st.selectbox(
